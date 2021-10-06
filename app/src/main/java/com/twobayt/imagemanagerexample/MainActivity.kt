5ed9c12e9dc2ce7677e370458f12931ca0507169
gurayakyuz <gur.akyuz@gmail.com>
2021-10-06 11:35:56 +0300

    Initial commit

---
+++ app/src/main/java/com/twobayt/imagemanagerexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.twobayt.imagemanagerexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
