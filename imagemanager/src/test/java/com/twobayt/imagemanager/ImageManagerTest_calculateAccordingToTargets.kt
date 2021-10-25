38734dc30f8ddd01a4edcd603f96e428ad19ea43
gurayakyuz <gur.akyuz@gmail.com>
2021-10-25 11:58:26 +0300

    tests updated

--- imagemanager/src/test/java/com/twobayt/imagemanager/ImageManagerTest_calculateAccordingToTargets.kt
+++ imagemanager/src/test/java/com/twobayt/imagemanager/ImageManagerTest_calculateAccordingToTargets.kt
@@ -1,7 +1,5 @@
 package com.twobayt.imagemanager
 
-import android.content.Context
-import androidx.test.core.app.ApplicationProvider
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,33 +10,24 @@ class ImageManagerTest_calculateAccordingToTargets {
 
     @Test
     fun checkTargetFunction() {
-        val context: Context = ApplicationProvider.getApplicationContext()
-
-        val height = 600
-        val width = 700
-
-        val manager = ImageManager.Builder(context)
-            .crop(true)
-            .targetHeight(height)
-            .targetWidth(width)
-            .build()
-
+        val targetHeight = 600.0f
+        val targetWidth = 700.0f
 
         //check height width is lower than target
-        val testResult1 = manager.calculateAccordingToTargets(500.0f, 1000.0f)
-        assertEquals(testResult1[1], height.toFloat())
+        val testResult1 = BitmapUtils.calculateAccordingToTargets(width = 500.0f, height = 1000.0f, targetWidth, targetHeight)
+        assertEquals(testResult1[1], targetHeight)
 
 
         //check width,  height is lower than target
-        val testResult2 = manager.calculateAccordingToTargets(1000.0f, 400.0f)
-        assertEquals(testResult2[0], width.toFloat())
+        val testResult2 = BitmapUtils.calculateAccordingToTargets(1000.0f, 400.0f, targetWidth, targetHeight)
+        assertEquals(testResult2[0], targetWidth)
 
 
-        val testResult3 = manager.calculateAccordingToTargets(2000.0f, 2000.0f)
-        assertEquals(testResult3[0]==width.toFloat()  || testResult3[1]==height.toFloat() , true)
+        val testResult3 = BitmapUtils.calculateAccordingToTargets(2000.0f, 2000.0f, targetWidth, targetHeight)
+        assertEquals(testResult3[0]== targetWidth || testResult3[1]== targetHeight, true)
 
         //too small dimens nothing changed
-        val testResult4 = manager.calculateAccordingToTargets(100.0f, 100.0f)
+        val testResult4 = BitmapUtils.calculateAccordingToTargets(100.0f, 100.0f, targetWidth, targetHeight)
         assertEquals(testResult4[0]==100.0f && testResult4[1]==100.0f , true)
 
     }
